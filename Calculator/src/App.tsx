163106e493c9f1ccfce7ad5d31d58ac10b7c2260
Wayne <wayne23123@gmail.com>
2025-05-21 10:44:15 +0800

    Added calculator project

---
+++ Calculator/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="p-8 bg-gray-100 text-center text-blue-500 text-xl font-bold">
+          Tailwind 測試
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
